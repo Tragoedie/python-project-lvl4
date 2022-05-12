efa9936c4eff63e3b79ff4e1ab76ef77bb11823f
Tragoedia <tragoedia@mail.ru>
2022-05-12 23:03:26 +0700

    heroku

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pn70n=fk(i018ah-iq2s8--*su(bm&ebzo4yj$39#&0x=z*^4m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
