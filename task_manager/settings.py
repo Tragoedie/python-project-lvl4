459289e9660ee12910eb9a07ab57fe50dea11696
Tragoedia <tragoedia@mail.ru>
2022-05-13 16:29:13 +0700

    heroku

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'task_manager',
-    'whitenoise.runserver_nostatic',
+#    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
